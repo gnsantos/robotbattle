c1e19ab9cccf995c4d381d0f8ad5fb881030240e
Gervasio Santos <gervasio.neto94@gmail.com>
2013-12-04 15:43:20 -0200

    Arrumando aqruivos

--- DebugJav.java
+++
@@ -1,8 +0,0 @@
-public class DebugJav{
-	public static void sayCrash(String argument){
-		System.out.println("------------------------------------------");
-		System.out.println("Program crashed HERE  : " + argument);
-		System.out.println("------------------------------------------");
-	}
-}
-
--- StackableInterface.java
+++
@@ -1,7 +0,0 @@
-import java.lang.*;
-import java.util.*;
-
-interface StackableInterface{
-	public void printYourself();
-	public double getValue();
-}
